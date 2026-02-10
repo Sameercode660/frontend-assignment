216cab185909030751d9dadf75ed0f462949b212
Mohd Sameer <mesh789736@gmail.com>
2026-02-10 19:10:42 +0530

    dashboard api

--- lib/dashboard.ts
+++ lib/dashboard.ts
@@ -1,7 +1,10 @@
 export async function getDashboardData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL?/api/dashboard}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/dashboard`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) throw new Error("Failed to fetch dashboard data");
 
