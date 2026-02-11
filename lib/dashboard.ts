e8b14f552d86efbc69091c90fd21131a7c32ad6a
Mohd Sameer <mesh789736@gmail.com>
2026-02-11 17:54:44 +0530

    dashboard function changes

--- lib/dashboard.ts
+++ lib/dashboard.ts
@@ -1,7 +1,5 @@
 export async function getDashboardData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/dashboard`, {
-    next: { revalidate: 10 },
-  });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/dashboard`);
 
   if (!res.ok) throw new Error("Failed to fetch dashboard data");
   return res.json();
