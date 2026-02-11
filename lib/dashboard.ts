417e243945f6da5c4c24f024ba1590036649e9c9
Mohd Sameer <mesh789736@gmail.com>
2026-02-11 18:07:26 +0530

    fix dashboard function

--- lib/dashboard.ts
+++ lib/dashboard.ts
@@ -1,5 +1,7 @@
 export async function getDashboardData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/dashboard`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/dashboard`, {
+    cache: "no-store",
+  });
 
   if (!res.ok) throw new Error("Failed to fetch dashboard data");
   return res.json();
